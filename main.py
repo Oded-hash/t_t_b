6e72a0da335cc790cbbe65cf86318fb6e2475b6d
Oded-hash <kroded@gmail.com>
2025-04-22 17:22:13 +0300

    port change at main.py

--- main.py
+++ main.py
@@ -73,4 +73,5 @@ if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.run_until_complete(set_webhook())
 
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
